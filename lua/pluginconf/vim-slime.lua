fef7eb037a2aeb1d836f0018a51e0e10aacdbab8
Juan Lara <julara@unal.edu.co>
2023-04-28 12:33:41 -0500

    Changed slime config

--- lua/pluginconf/vim-slime.lua
+++ lua/pluginconf/vim-slime.lua
@@ -1,23 +1,10 @@
 vim.g.slime_target = "tmux"
 vim.g.slime_paste_file = vim.fn.expand("$HOME/.slime_paste")
 
+local delimiters = {
+    python = "# %%", sh = "# %%", julia = "# %%", r = "# %%", javascript = "// %%",
+    lua = "-- %%", haskell="-- %%", lisp = ";; %%"
+}
+
 local ft = vim.bo.filetype
-if (
-    ft == "python" or
-    ft == "sh" or
-    ft == "julia" or
-    ft == "r"
-    ) then
-    vim.g.slime_cell_delimiter = "# %%"
-elseif (ft == "javascript") then
-    vim.g.slime_cell_delimiter = "// %%"
-elseif (
-    ft == "lua" or
-    ft == "haskell"
-    ) then
-    vim.g.slime_cell_delimiter = "-- %%"
-elseif (
-    ft == "lisp"
-    ) then
-    vim.g.slime_cell_delimiter = ";; %%"
-end
+vim.g.slime_cell_delimiter = delimiters[ft]
